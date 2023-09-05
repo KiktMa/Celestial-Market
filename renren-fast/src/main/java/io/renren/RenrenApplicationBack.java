ee284d9bab6f8e4bca023dfc1dbc1f2c10020bd5
majun <2113725201@qq.com>
2023-09-05 20:42:34 +0800

    添加阿里云OSS服务的starter以及controller

--- renren-fast/src/main/java/io/renren/RenrenApplicationBack.java
+++ renren-fast/src/main/java/io/renren/RenrenApplicationBack.java
@@ -15,7 +15,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class RenrenApplication {
+public class RenrenApplicationBack {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RenrenApplication.class, args);
