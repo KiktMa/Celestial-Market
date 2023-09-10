591fd928d462bd42d79e214087329de8d49835cf
majun <2113725201@qq.com>
2023-09-10 17:13:49 +0800

    属性和属性分组关联相关增删改查

--- gulimall-product/src/main/java/com/mjtal/common/product/service/AttrAttrgroupRelationService.java
+++ gulimall-product/src/main/java/com/mjtal/common/product/service/AttrAttrgroupRelationService.java
@@ -1,9 +1,11 @@
 package com.mjtal.common.product.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.mjtal.common.product.vo.AttrGroupRelationVo;
 import com.mjtal.common.utils.PageUtils;
 import com.mjtal.common.product.entity.AttrAttrgroupRelationEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,5 +18,7 @@ import java.util.Map;
 public interface AttrAttrgroupRelationService extends IService<AttrAttrgroupRelationEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveBatch(List<AttrGroupRelationVo> vos);
 }
 
--- gulimall-product/src/main/java/com/mjtal/common/product/service/AttrGroupService.java
+++ gulimall-product/src/main/java/com/mjtal/common/product/service/AttrGroupService.java
@@ -1,9 +1,11 @@
 package com.mjtal.common.product.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.mjtal.common.product.vo.AttrGroupWithAttrsVo;
 import com.mjtal.common.utils.PageUtils;
 import com.mjtal.common.product.entity.AttrGroupEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,5 +20,7 @@ public interface AttrGroupService extends IService<AttrGroupEntity> {
     PageUtils queryPage(Map<String, Object> params);
 
     PageUtils queryPage(Map<String, Object> params, Long catelogId);
+
+    List<AttrGroupWithAttrsVo> getAttrGroupWithAttrsByCatelogId(Long catelogId);
 }
 
