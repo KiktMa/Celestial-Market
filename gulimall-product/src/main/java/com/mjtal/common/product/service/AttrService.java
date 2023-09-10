7dbc2aa1cb3e57f9489982847ca552be8bac4313
majun <2113725201@qq.com>
2023-09-10 16:01:40 +0800

    商品属性的增删改查

--- gulimall-product/src/main/java/com/mjtal/common/product/service/AttrService.java
+++ gulimall-product/src/main/java/com/mjtal/common/product/service/AttrService.java
@@ -1,9 +1,13 @@
 package com.mjtal.common.product.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.mjtal.common.product.vo.AttrGroupRelationVo;
+import com.mjtal.common.product.vo.AttrRespVo;
+import com.mjtal.common.product.vo.AttrVo;
 import com.mjtal.common.utils.PageUtils;
 import com.mjtal.common.product.entity.AttrEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,5 +20,19 @@ import java.util.Map;
 public interface AttrService extends IService<AttrEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveAttr(AttrVo attr);
+
+    PageUtils queryBaseAttrPage(Map<String, Object> params, Long catelogId, String type);
+
+    AttrRespVo getAttrInfo(Long attrId);
+
+    void updateAttr(AttrVo attr);
+
+    List<AttrEntity> getRelationAttr(Long attrgroupId);
+
+    void deleteRelation(AttrGroupRelationVo[] vos);
+
+    PageUtils getNoRelationAttr(Map<String, Object> params, Long attrgroupId);
 }
 
