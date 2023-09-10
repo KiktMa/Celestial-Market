d31432262fb774c861d9437d7d50d2d1a06df8d1
majun <2113725201@qq.com>
2023-09-10 17:09:58 +0800

    品牌分类关联相关增删改查相关

--- gulimall-product/src/main/java/com/mjtal/common/product/service/CategoryBrandRelationService.java
+++ gulimall-product/src/main/java/com/mjtal/common/product/service/CategoryBrandRelationService.java
@@ -1,9 +1,11 @@
 package com.mjtal.common.product.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.mjtal.common.product.entity.BrandEntity;
 import com.mjtal.common.utils.PageUtils;
 import com.mjtal.common.product.entity.CategoryBrandRelationEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,5 +18,13 @@ import java.util.Map;
 public interface CategoryBrandRelationService extends IService<CategoryBrandRelationEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveDetail(CategoryBrandRelationEntity categoryBrandRelation);
+
+    void updateBrand(Long brandId, String name);
+
+    void updateCategory(Long catId, String name);
+
+    List<BrandEntity> getBrandsByCatId(Long catId);
 }
 
