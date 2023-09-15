747725631074a9e26651e21f6a382805cd34a1dd
majun <2113725201@qq.com>
2023-09-15 16:15:17 +0800

    coupon模块增删改查修改

--- gulimall-coupon/src/main/java/com/mjtal/gulimall/coupon/controller/SkuFullReductionController.java
+++ gulimall-coupon/src/main/java/com/mjtal/gulimall/coupon/controller/SkuFullReductionController.java
@@ -3,12 +3,9 @@ package com.mjtal.gulimall.coupon.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.mjtal.common.to.SkuReductionTo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.mjtal.gulimall.coupon.entity.SkuFullReductionEntity;
 import com.mjtal.gulimall.coupon.service.SkuFullReductionService;
@@ -29,6 +26,13 @@ import com.mjtal.common.utils.R;
 public class SkuFullReductionController {
     @Autowired
     private SkuFullReductionService skuFullReductionService;
+
+    @PostMapping("/saveinfo")
+    public R saveInfo(@RequestBody SkuReductionTo reductionTo){
+
+        skuFullReductionService.saveSkuReduction(reductionTo);
+        return R.ok();
+    }
 
     /**
      * 列表
--- gulimall-coupon/src/main/java/com/mjtal/gulimall/coupon/service/SkuFullReductionService.java
+++ gulimall-coupon/src/main/java/com/mjtal/gulimall/coupon/service/SkuFullReductionService.java
@@ -1,6 +1,7 @@
 package com.mjtal.gulimall.coupon.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.mjtal.common.to.SkuReductionTo;
 import com.mjtal.common.utils.PageUtils;
 import com.mjtal.gulimall.coupon.entity.SkuFullReductionEntity;
 
@@ -16,5 +17,7 @@ import java.util.Map;
 public interface SkuFullReductionService extends IService<SkuFullReductionEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveSkuReduction(SkuReductionTo reductionTo);
 }
 
