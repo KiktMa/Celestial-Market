209d5802e18809c7e34a4f2fb7e011b528ba23a9
majun <2113725201@qq.com>
2023-09-17 14:22:15 +0800

    故里商店基础篇开发完成

--- gulimall-ware/src/main/java/com/mjtal/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/mjtal/gulimall/ware/GulimallWareApplication.java
@@ -3,9 +3,10 @@ package com.mjtal.gulimall.ware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallWareApplication {
