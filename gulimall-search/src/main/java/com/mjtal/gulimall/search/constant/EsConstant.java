92dbd5e819270eb57a89b3c112b8df83d7c695bc
majun <2113725201@qq.com>
2023-10-11 17:25:16 +0800

    Redis缓存相关

--- gulimall-search/src/main/java/com/mjtal/gulimall/search/constant/EsConstant.java
+++ gulimall-search/src/main/java/com/mjtal/gulimall/search/constant/EsConstant.java
@@ -5,5 +5,7 @@ package com.mjtal.gulimall.search.constant;
  * @Date: 2023/9/26 15:21
  */
 public class EsConstant {
-    public static final String PRODUCT_INDEX="product"; // sku数据在es中的索引
+    public static final String PRODUCT_INDEX="gulimall_product"; // sku数据在es中的索引
+
+    public static final Integer PRODUCT_PAGESIZE = 16;
 }
