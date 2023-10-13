98b1df409a02fe0c72247fca78cc9c37912022fd
majun <2113725201@qq.com>
2023-10-13 23:17:19 +0800

    增加登录认证模块，包括第三方登录

---
+++ gulimall-auth-server/src/main/java/com/mjtal/gulimall/auth/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.mjtal.gulimall.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Author: june
+ * @Date: 2023/10/11 10:45
+ */
+
+@Controller
+public class LoginController {
+
+//    @GetMapping("/login.html")
+//    public String loginPage(){
+//
+//        return "login";
+//    }
+//
+//    @GetMapping("/reg.html")
+//    public String regPage(){
+//
+//        return "reg";
+//    }
+}
